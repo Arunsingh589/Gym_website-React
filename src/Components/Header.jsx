1e634bcb7f2752259c0cd48f54a8d717c4a799f6
Arun Singh <as3945787@gmail.com>
2024-02-19 12:33:11 +0530

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div id='main'>
+        <div className="name">
+            <h2>STEP UP YOUR</h2>
+            <h1><span>FITFOOD</span> SUPPLIMENT</h1>
+            <p className="details">Build YOur Body And Fitness With Professional 
+            Touch And Guide Pankaj Fitness</p>
+            <div className="header-btns">
+                <a href="#" className='header-btn'>JOIN US</a>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
